cc4ece1f4b9466278cefaa52920127115b3a1e35
Hector A. Mantellini <xombra@users.noreply.github.com>
2016-01-16 14:09:18 -0430

    bugfixed

--- prefs.js
+++ prefs.js
@@ -137,7 +137,7 @@ pref("webgl.disabled", true);
 
 pref("network.http.referer.trimmingPolicy", 1);
 pref("network.http.referer.XOriginPolicy", 1);
-pref("network.http.sendRefererHeader", 0);
+pref("network.http.sendRefererHeader", 1);
 
 // deshabilitar completamente Hello
 
